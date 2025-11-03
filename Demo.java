4a3ed7a60152e310997632607c0a17b0267bdac5
P K KAVYA <132293399+Kavya-918@users.noreply.github.com>
2025-11-03 07:20:19 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -7,7 +7,22 @@ void main(){
 }
 
 
-
+//VS code enable preview steps & code execution
+// PS D:\Java21> javac Demo.java
+// Demo.java:4: error: unnamed classes are a preview feature and are disabled by default.
+// void main(){
+// ^
+//   (use --enable-preview to enable unnamed classes)
+// 1 error
+// PS D:\Java21> javac --enable-preview Demo.java
+// error: --enable-preview must be used with either -source or --release
+// PS D:\Java21> javac --enable-preview --release 21 Demo.java
+// Note: Recompile with -Xlint:preview for details.
+// PS D:\Java21> java Demo
+// Error: LinkageError occurred while loading main class Demo
+//         java.lang.UnsupportedClassVersionError: Preview features are not enabled for Demo (class file version 65.65535). Try running with '--enable-preview'
+// PS D:\Java21> java --enable-preview Demo
+// Hello
 
 
 
