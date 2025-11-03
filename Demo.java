6e21e00703de286df234a1a447a60e387351ed58
P K KAVYA <132293399+Kavya-918@users.noreply.github.com>
2025-11-03 07:19:29 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,21 @@
+
+//Unnamed classes & instance main method
+//No need for public class or static main  methods --beginner friendly 
+//Preview feature in JDK21 & JDK 25 it became standard
+void main(){
+    System.out.println("Hello");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
